98f156e14580f4a0fb086c43413ad3cb673276e8
kasacchiful <kasacchiful@gmail.com>
2020-08-07 15:02:20 +0900

    BodyPixモデルのロード (tfjsをv2からv1にダウングレード)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import "babel-polyfill";
+import * as bodyPix from '@tensorflow-models/body-pix';
+import * as tf from '@tensorflow/tfjs-core';
+import {toggleLoadingUI} from "./utils.js"
+
+const state = {
+  net: null
+}
+
+async function loadBodyPix() {
+  toggleLoadingUI(true);
+  state.net = await bodyPix.load({
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    multiplier: 0.75,
+    quantBytes: 2
+  });
+  toggleLoadingUI(false);
+}
 
 export async function bindPage() {
+  await loadBodyPix();
   document.getElementById('loading').style.display = 'none';
   document.getElementById('main').style.display = 'inline-block';
 }
